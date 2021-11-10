668737f7d1c4de9788a98bd1d12b0488c47fecf1
Emma <emma@madetech.com>
2021-11-10 15:06:04 +0000

    Using numpys matrix mult

--- src/neuron.py
+++ src/neuron.py
@@ -6,16 +6,20 @@ def calculate_single_neuron_in_layer(inputs, weights, bias):
 
 
 if __name__ == '__main__':
+    # i
     inputs = [1, 2, 3, 2.5]
 
+    # W
     weights = [
         [0.2, 0.8, -0.5, 1],
         [0.5, -0.91, 0.26, -0.5],
         [-0.26, -0.27, 0.17, 0.87]
     ]
 
+    # b
     biases = [2, 3, 0.5]
 
-    outputs = [calculate_single_neuron_in_layer(inputs, w, b) for w, b in zip(weights, biases)]
+    # W i^T + b
+    outputs = np.matmul(weights, inputs) + biases
 
     print(outputs)
